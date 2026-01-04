1146aaee7e22f560592e24e69f9352f6b71c4d69
Luis <lamadoalvarez@gmail.com>
2026-01-04 16:38:08 -0600

    slight fix to input timings

--- src/engine/io/Input.cpp
+++ src/engine/io/Input.cpp
@@ -118,7 +118,7 @@ bool Input::IsJustPressed(int input) {
 }
 
 bool Input::IsJustDoublePressed(int input) {
-  return IsJustPressed(input) && s_timeSinceLastInput.count(input) && s_timeSinceLastInput[input] > 0.0f;
+  return IsJustPressed(input) && s_timeSinceLastInput.count(input) && s_timeSinceLastInput[input] > 0.02f;
 }
 
 void Input::Init(Window* window) {
